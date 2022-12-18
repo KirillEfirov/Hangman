17465da54e9af60930ca2007b9fb3dd26b182cb2
Kirill <shishkokirill.fudosin@gmail.com>
2022-12-18 06:58:48 +0300

    User can guess word

--- Player.rb
+++ Player.rb
@@ -1,5 +1,19 @@
 class Player
     def initialize
-        
+        @try = 0
+        @attempt
+        @guesses = Array.new
+    end
+
+    def guess_letter
+        @attempt = gets.chomp!
+        @try += 1
+        @guesses.push(@attempt)
+    end
+
+    def guess_word
+    end
+
+    def save_game?
     end
 end
