a07ec3fb01c9a75fefd621a94e9ed5d96c94a754
Kirill <shishkokirill.fudosin@gmail.com>
2022-12-18 05:36:37 +0300

    Create array of words, reading them from file

--- Computer.rb
+++ Computer.rb
@@ -1,5 +1,48 @@
 class Computer
     def initialize
-        
+        @dictionary = Array.new
+        @wrong_letters = Array.new
+    end
+
+    public
+    def create_dictionary(file)
+        @dictionary = read_content(file)
+    end
+
+    def show_dictionary
+        puts @dictionary
+    end
+
+    private
+    def read_content(file)
+        File.open(file, "r") do |content|
+            content.readlines
+        end
+    end
+
+    private
+    def is_letter_true?(letter)
+
+    end
+
+    public
+    def count_letters(word)
+
+    end
+
+    def display_word(word)
+
+    end
+
+    #if is_letter_true? returns true then insert the letter in a word
+    def insert_right_letter(word)
+
+    end
+
+    def add_wrong_letter(letter)
+    end
+
+    def display_wrong_letters()
+
     end
 end
