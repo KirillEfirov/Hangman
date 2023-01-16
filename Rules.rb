8bde5ae856de3a36fcb1750f928a179ab3f78319
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-16 22:19:09 +0300

    create class with the rules of the game

---
+++ Rules.rb
@@ -0,0 +1,9 @@
+class Rules
+    def show_rules
+        puts "Welcome to Hangman. At the beginning of each game a randomly selected word will be chosen," 
+        puts "you'll be allowed to guess letters until you miss 7 times. If you solve the word correctly you win."
+        puts "At the beginning you will be given the option to load a previously saved game and before each guess"
+        puts "you will be presented with an opportunity to save your game"
+        puts "---------"
+      end 
+end
