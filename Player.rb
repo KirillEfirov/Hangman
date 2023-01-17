6bbfbd17d81983c486b629c87c8bed184aeecf15
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-17 21:25:25 +0300

    create getter to return guesses of the player

--- Player.rb
+++ Player.rb
@@ -1,20 +1,15 @@
-require "yaml"
-#Number of attempts, all the letters should be serialized
-
 class Player
     def initialize
-        @try = 0
-        @guesses = Array.new
+      @try = 0
+      @guesses = Array.new
     end
 
     def guess_letter
-        @try += 1
-        @guesses.push(gets.chomp!)
+      @try += 1
+      @guesses.push(gets.chomp!)
     end
 
-    def guess_word
-    end
-
-    def save_game?
+    def get_guesses
+      @guesses
     end
 end
