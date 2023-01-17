2f12b0bd25042a5ce14bbe92d405297b1d6ecea3
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-17 20:00:31 +0300

    player has opportunity to save to and load game from game.yaml

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative "Computer.rb"
 require_relative "Player.rb"
 require_relative "Rules"
+require_relative "Games.rb"
 require "yaml"
 
 rules = Rules.new
@@ -16,40 +17,69 @@ when "1"
   player = Player.new
 
   computer.choose_word("words.txt")
-  p computer.get_guessed_word           #Don't forget to remove it from player later
+  p computer.get_guessed_word                                   #Don't forget to remove it from player later
   computer.create_filled_word
   print "Guessed word: "
   computer.display_filled_word
 
   puts
 
+  puts "-" * 80
+  puts "If you want to save a current game type 'save' at any moment"
+  puts "-" * 80
+
   until computer.get_wrong_letters.size == 7 do
-    print "Try to guess letter: "
+
+    print "Try to guess letter or save a game: "
+    
     player.guess_letter
-    last_guess = player.get_guesses.last
 
-    if computer.is_letter_true?(last_guess)
-      puts "Letter true"
-      computer.insert_right_letter(last_guess)
-      computer.display_filled_word
-      puts
+    if player.get_guesses.last == "save"
+      player.get_guesses.pop()                                   #pop last elem in order not to save the word 'save' in player's attempts 
 
-      unless computer.is_player_win?
-        puts "You cracked the word"
-        exit
+      game = Games.new(player, computer)
+
+      File.open("game.yaml", "w") do |file| 
+        file.write(game.to_yaml)
       end
-    else 
-      puts "Letter false"
-      computer.add_wrong_letter(last_guess)
+      exit
+    else
+      last_guess = player.get_guesses.last
+
+      if computer.is_letter_true?(last_guess)
+        puts "Letter true"
+        computer.insert_right_letter(last_guess)
+        computer.display_filled_word
+        puts
+
+        unless computer.is_player_win?
+          puts "You cracked the word"
+          exit
+        end
+      else 
+        puts "Letter false"
+        computer.add_wrong_letter(last_guess)
+      end
+
     end
   end
 
-  puts "Better luck next time"
+  p "Better luck next time"
 
 
-  #File.open("player.yaml", "a") { |file| file.write(YAML::dump(player)) }
 when "2"
-  exit
+  data_in_yaml = String.new
+  File.open("game.yaml", "r") do |file|
+    data_in_yaml = file.read
+  end
+
+  deserialized_data = Games.from_yaml(data_in_yaml)
+
+  p deserialized_data
+  player = deserialized_data[0]
+  computer = deserialized_data[1]
+
+
 else
   exit
 end
